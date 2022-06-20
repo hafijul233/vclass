91fd5d9a26e29656cb14e9bf8ba426dcbe328933
Hafijul Islam <hafijul233@gmail.com>
2022-06-21 00:28:15 +0600

    file identifier added

---
+++ src/common/repositories/model.repository.ts
@@ -0,0 +1,58 @@
+/*import { plainToClass } from 'class-transformer';
+import { DeepPartial, Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { ModelSerializer } from '@app/common/serializers/model.serializer';
+
+export class ModelRepository<
+  T,
+  K extends ModelSerializer,
+> extends Repository<T> {
+  async showEntity(
+    id: number,
+    relations: string[] = [],
+    throwsException = false,
+  ): Promise<K | null> {
+    return await this.findOne({
+      where: { id },
+      relations,
+    })
+      .then((entity) => {
+        if (!entity && throwsException) {
+          return Promise.reject(new NotFoundException('Entity not found.'));
+        }
+
+        return Promise.resolve(entity ? this.transform(entity) : null);
+      })
+      .catch((error) => Promise.reject(error));
+  }
+
+  async createEntity(
+    inputs: DeepPartial<T>,
+    relations: string[] = [],
+  ): Promise<K> {
+    return this.save(inputs)
+      .then(
+        async (entity) => await this.showEntity((entity as any).id, relations),
+      )
+      .catch((error) => Promise.reject(error));
+  }
+
+  async updateEntity(
+    id: number,
+    inputs: QueryDeepPartialEntity<T>,
+    relations: string[] = [],
+  ): Promise<K> {
+    return this.update(id, inputs)
+      .then(async () => await this.showEntity(id, relations))
+      .catch((error) => Promise.reject(error));
+  }
+
+  transform(model: T, transformOptions = {}): K {
+    return plainToClass(ModelSerializer, model, transformOptions) as K;
+  }
+
+  transformMany(models: T[], transformOptions = {}): K[] {
+    return models.map((model) => this.transform(model, transformOptions));
+  }
+}*/
--- src/providers/database/provider.module.ts
+++ src/providers/database/provider.module.ts
@@ -9,30 +9,27 @@ import { DatabaseConfigService } from '@app/config/database/config.service';
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [DatabaseConfigModule],
-      useFactory: async (databaseConfig: DatabaseConfigService) => {
-        return {
-          type: databaseConfig.connection as DatabaseType,
-          host: databaseConfig.host,
-          port: databaseConfig.port,
-          username: databaseConfig.username,
-          password: databaseConfig.password,
-          database: databaseConfig.database,
-          autoLoadEntities: true,
-          /*entities: ['dist/modules/!**!/!*.entity.{ts,js}'], //pattern issue using dist is safe
-          subscribers: ['dist/modules/!**!/!*.subscriber.{ts,js}'], //pattern issue using dist is safe
-          migrations: ['dist/database/migrations/!*.js'], //pattern issue using dist is safe*/
-          logging: ['error'],
-          synchronize: false,
-          dropSchema: false,
-          namingStrategy: new SnakeNamingStrategy(),
-          migrationsTableName: 'migrations',
-          migrationsTransactionMode: 'each', //'all' is best on production
-          charset: 'UTF8MB4_UNICODE_CI',
-          cli: {
-            migrationsDir: 'src/database/migrations',
-          },
-        };
-      },
+      useFactory: async (databaseConfig: DatabaseConfigService) => ({
+        type: databaseConfig.connection as DatabaseType,
+        host: databaseConfig.host,
+        port: databaseConfig.port,
+        username: databaseConfig.username,
+        password: databaseConfig.password,
+        database: databaseConfig.database,
+        autoLoadEntities: true,
+        entities: [__dirname + '/../../**/*.entity.{ts,js}'],
+        subscribers: [__dirname + '/../../**/*.subscriber.{ts,js}'],
+        logging: ['error'],
+        synchronize: false,
+        dropSchema: false,
+        namingStrategy: new SnakeNamingStrategy(),
+        migrationsTableName: 'migrations',
+        migrationsTransactionMode: 'each', //'all' is best on production
+        charset: 'UTF8MB4_UNICODE_CI',
+        cli: {
+          migrationsDir: __dirname + '/../../database/migrations',
+        },
+      }),
       inject: [DatabaseConfigService],
     } as TypeOrmModuleAsyncOptions),
   ],
