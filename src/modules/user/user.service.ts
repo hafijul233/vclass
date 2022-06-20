3be60ec1248f07df0b95d6658c106e5940e88cbc
Hafijul Islam <hafijul233@gmail.com>
2022-06-21 00:53:41 +0600

    find user  dtoo added

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { FindUserDto } from '@app/modules/user/dto/find-user.dto';
 
 @Injectable()
 export class UserService {
@@ -12,23 +13,50 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async findAll(findUserDto: FindUserDto): Promise<User[]> {
+    return await this.userRepository.find(findUserDto);
   }
 
-  async findAll(): Promise<User[]> {
-    return await this.userRepository.find();
+  async findOne(id: number, relations: string[] = [], throwsException = false) {
+    return await this.userRepository
+      .findOne({
+        where: { id },
+        relations,
+      })
+      .then((entity) => {
+        if (!entity && throwsException) {
+          return Promise.reject(new NotFoundException('Entity not found.'));
+        }
+
+        return Promise.resolve(entity ? entity : null);
+      })
+      .catch((error) => Promise.reject(error));
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async create(createUserDto: CreateUserDto, relations: string[] = []) {
+    return await this.userRepository
+      .save(createUserDto)
+      .then(async (entity) => await this.findOne((entity as any).id, relations))
+      .catch((error) => Promise.reject(error));
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+    relations: string[] = [],
+  ) {
+    return await this.userRepository
+      .update(id, updateUserDto)
+      .then(async () => await this.findOne(id, relations))
+      .catch((error) => Promise.reject(error));
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    return await this.userRepository
+      .softDelete(id)
+      .then(async () => {
+        return (await this.findOne(id)) == null;
+      })
+      .catch((error) => Promise.reject(error));
   }
 }
